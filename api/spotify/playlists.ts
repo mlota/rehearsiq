3ac09f7c37ad333ad50d33f0eb710e7959a088fe
Mel Lota <melvin_lota@hotmail.com>
2025-02-26 20:59:01 +0000

    chore: Adds spotify functions

--- api/spotify/playlists.ts
+++ api/spotify/playlists.ts
@@ -1,7 +1,20 @@
 // import { getAccessToken } from '@rehearsiq/api';
-import { getAccessToken } from '../../libs/api/src/lib/spotify';
+import { getAccessToken, getPlaylist } from '../../libs/api/src/lib/spotify';
 
 export async function GET(request: Request) {
+	// Get the URL from the request
+	const url = new URL(request.url);
+
+	// Extract the id from the query parameters
+	const id = url.searchParams.get('id');
+
+	// Check if ID is provided
+	if (!id) {
+		return Response.json({ error: 'Missing id parameter' }, { status: 400 });
+	}
+
 	const accessToken = await getAccessToken();
-	return new Response(accessToken);
+	const playlist = await getPlaylist(accessToken, id);
+
+	return Response.json(playlist);
 }
--- libs/api/src/lib/spotify.ts
+++ libs/api/src/lib/spotify.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { AccessToken } from 'spotify-types';
+import { AccessToken, Playlist } from 'spotify-types';
 
 // Spotify API endpoints
 const TOKEN_ENDPOINT = 'https://accounts.spotify.com/api/token';
@@ -17,16 +17,35 @@ export const getAccessToken = async (): Promise<string> => {
 		const response = await axios.post<AccessToken>(
 			TOKEN_ENDPOINT,
 			`grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`,
-			{
-				headers: {
-					'Content-Type': 'application/x-www-form-urlencoded'
-				}
-			}
+			{ headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
 		);
 		return response.data.access_token;
 	} catch (error: any) {
 		throw new Error(
 			`Error getting access token: ${error.response?.data || error.message}`
+		);
+	}
+};
+
+export const getPlaylist = async (
+	accessToken: string,
+	playlistId: string
+): Promise<Playlist> => {
+	if (!playlistId) {
+		throw new Error('Missing Spotify playlist id');
+	}
+
+	try {
+		const response = await axios.get<Playlist>(
+			`${PLAYLIST_ENDPOINT}/${playlistId}`,
+			{
+				headers: { Authorization: `Bearer ${accessToken}` }
+			}
+		);
+		return response.data;
+	} catch (error: any) {
+		throw new Error(
+			`Error getting playlist: ${error.response?.data || error.message}`
 		);
 	}
 };
