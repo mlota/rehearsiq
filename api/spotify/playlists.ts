6c91b874cfaae84af29de71e296d968270e449dd
Mel Lota <melvin_lota@hotmail.com>
2025-02-26 19:56:37 +0000

    chore: Function testing

--- api/spotify/playlists.ts
+++ api/spotify/playlists.ts
@@ -1,4 +1,5 @@
-import { getAccessToken } from '@rehearsiq/api';
+// import { getAccessToken } from '@rehearsiq/api';
+import { getAccessToken } from '../../libs/api/src/lib/spotify';
 
 export async function GET(request: Request) {
 	const accessToken = await getAccessToken();
